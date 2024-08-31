e02829b1655b6e668176b71e14cfe46c6d775f4d
Igor Lemos <igor.neres-lemos@live.com>
2024-08-31 04:52:45 +0000

    Editions to complete and correct Mongoose challenge solutions

--- myApp.js
+++ myApp.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
+const mongoose = require('mongoose')
 
+async function run() {
+   mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+};
 
 let Person;
 
